31478fe98a68cd4ac341ab0070563f6a0efc4ab0
Amro Shekh Zain <amroshehzen@hotmail.com>
2023-10-08 20:47:29 +0300

    Add Navigation Component

--- app/src/main/java/com/applefish/littlelemon/HomeScreen.kt
+++ app/src/main/java/com/applefish/littlelemon/HomeScreen.kt
@@ -1,28 +1,15 @@
 package com.applefish.littlelemon
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.littlelemon.LowerPanel
 
 
 @Composable
-fun HomeScreen(navHostController:NavHostController) {
+fun HomeScreen(navHostController: NavHostController) {
     Column {
 /*        Column (modifier = Modifier.fillMaxWidth(),
             verticalArrangement = Arrangement.Center,
--- app/src/main/java/com/applefish/littlelemon/TestFeature.kt
+++ app/src/main/java/com/applefish/littlelemon/TestFeature.kt
@@ -1,47 +1,30 @@
 package com.applefish.littlelemon
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.DrawerState
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.NavigationDrawerItem
-import androidx.compose.material3.NavigationDrawerItemDefaults
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberDrawerState
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.applefish.littlelemon.ui.theme.LittleLemonTheme
 import com.applefish.littlelemon.ui.theme.Pink80
 import com.applefish.littlelemon.ui.theme.Purple80
@@ -110,39 +93,54 @@ fun DrawerContent() {
     }
 }
 
-
-val categories = listOf<String>("Lunch","Dinner","Main","Dessert")
-
-@Composable
-fun CategoryButton(category: String) {
-    Button(onClick = { /*TODO*/ },
-        colors = ButtonDefaults.buttonColors(containerColor= Color.LightGray),
-        shape = RoundedCornerShape(40),
-        modifier = Modifier.padding(5.dp)
-    ) {
-        Text(text = category,
-            color = Color.Black)
-    }
-}
-
 @Composable
 @Preview
-fun PrefiewCategoryButton(){
-    CategoryButton(category = "Cat")
-}
-
-@Composable
-@Preview
-fun ListOfCategory(){
-    Row {
-        categories.forEach {
-            CategoryButton(category =it)
+fun LazyGrid(){
+    LazyVerticalGrid(columns = GridCells.Fixed (2)) {
+        items(500){number ->
+            MyGridCell()
         }
     }
-
 }
 
+@Composable
+fun MyGridCell() {
+    Card(
+        elevation = CardDefaults.cardElevation(16.dp),
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.greeksalad),
+                contentDescription = "Greek Salad",
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Text(
+                text = "Greek Salad",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .align(Alignment.TopStart)
 
+            )
+            Text(
+                text = "$12.99",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .background(Color.White)
+                    .padding(start = 4.dp, end = 4.dp)
+                    .align(Alignment.BottomEnd)
 
-
-
+            )
+        }
+    }
+}
